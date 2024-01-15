b9108d8f89f4099b9aba78f46e361207a92cf531
Anastasiia <vasylchuk.anastasia@gmail.com>
2024-01-15 14:28:37 +0100

    Added chat api

---
+++ server/express/handlers/chat.js
@@ -0,0 +1,34 @@
+const fetch = require('cross-fetch');
+
+const API_URL = 'https://api.stackexchange.com/search/advanced';
+const SITE_DOMAIN = 'stackoverflow.com';
+
+module.exports = function (req, res) {
+  const message = req.query['q'];
+  const keywords = extractKeywords(message);
+
+  fetch(`${API_URL}?site=${SITE_DOMAIN}&q=${keywords}`)
+    .then((apires) => {
+      if (apires.status >= 400) {
+        throw new Error('Bad response from server');
+      }
+      return apires.json();
+    })
+    .then((data) => {
+      console.log(data['quota_remaining']);
+      res.json(extractAnswer(data));
+
+    })
+
+};
+
+function extractKeywords(message) {
+  // TODO: Implement keyword extraction logic here
+  return message;
+}
+
+function extractAnswer(data) {
+  // TODO: Implement answer extraction logic here
+  console.log(data);
+  return data;
+}
